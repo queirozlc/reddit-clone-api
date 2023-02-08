98bc53cc7ee5387e9c14e8ca874c40aa1d840f97
Lucas :) <91512054+queirozlc@users.noreply.github.com>
2023-02-08 17:24:38 -0300

    feat: Regex for DTO username.

--- src/main/java/com/lucas/redditclone/dto/request/user/UserRequest.java
+++ src/main/java/com/lucas/redditclone/dto/request/user/UserRequest.java
@@ -2,6 +2,7 @@ package com.lucas.redditclone.dto.request.user;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -18,6 +19,9 @@ public class UserRequest {
 	@NotBlank(message = "the field 'name' is required")
 	private String name;
 	@NotBlank(message = "the field 'username' is required")
+	@Size(min = 6, message = "the field 'username' must be at least {min} characters")
+	@Pattern(regexp = "(?:@)([A-Za-z0-9_](?:(?:[A-Za-z0-9_]|(?:\\.(?!\\.))){0,28}(?:[A-Za-z0-9_]))?)",
+			message = "the field 'username' must be a valid username. Ex: @name.lastname")
 	private String username;
 	@NotBlank(message = "the field 'email' is required")
 	@Email(message = "the field 'email' must be a valid email")
