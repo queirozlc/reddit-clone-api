54601d346f94ee7875aedb197a55fb02767cbeff
Lucas :) <91512054+queirozlc@users.noreply.github.com>
2023-02-08 17:21:15 -0300

    feat: Email method is an async one

--- src/main/java/com/lucas/redditclone/service/impl/EmailService.java
+++ src/main/java/com/lucas/redditclone/service/impl/EmailService.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -16,6 +17,7 @@ import java.time.LocalDateTime;
 public class EmailService {
 	private final JavaMailSender mailSender;
 
+	@Async
 	public void sendEmail(@Valid MailResponseBody mailResponseBody) {
 		mailResponseBody.setSendDateTime(LocalDateTime.now());
 		try {
