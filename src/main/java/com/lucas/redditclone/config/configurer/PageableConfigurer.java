7041b7210a0c8fab6f872dceb87bfe4478e1f2df
Lucas :) <91512054+queirozlc@users.noreply.github.com>
2023-02-13 15:02:04 -0300

    feat: Pageable default config

---
+++ src/main/java/com/lucas/redditclone/config/configurer/PageableConfigurer.java
@@ -0,0 +1,19 @@
+package com.lucas.redditclone.config.configurer;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class PageableConfigurer implements WebMvcConfigurer {
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+		var resolver = new PageableHandlerMethodArgumentResolver();
+		resolver.setFallbackPageable(PageRequest.of(0, 10));
+		resolvers.add(resolver);
+	}
+}
