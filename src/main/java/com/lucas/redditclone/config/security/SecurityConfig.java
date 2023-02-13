890e6269e986839d825a25cb3ed5c871ee709d18
Lucas :) <91512054+queirozlc@users.noreply.github.com>
2023-02-13 15:01:46 -0300

    refactor: All endpoints permitted for development

--- src/main/java/com/lucas/redditclone/config/security/SecurityConfig.java
+++ src/main/java/com/lucas/redditclone/config/security/SecurityConfig.java
@@ -15,8 +15,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
-
 @EnableWebSecurity
 @Configuration
 @RequiredArgsConstructor
@@ -33,8 +31,7 @@ public class SecurityConfig {
 				.csrf()
 				.disable()
 				.authorizeHttpRequests()
-				.requestMatchers(antMatcher("/api/auth/**")).permitAll()
-				.anyRequest().authenticated()
+				.anyRequest().permitAll()
 				.and()
 				.authenticationProvider(authenticationProvider())
 				.addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class)
