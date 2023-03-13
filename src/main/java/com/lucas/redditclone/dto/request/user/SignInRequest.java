392967178c3835e3e92410fa87b17afd415e3a9d
Lucas :) <91512054+queirozlc@users.noreply.github.com>
2023-03-13 15:43:05 -0300

    refactor: username pattern changes

--- src/main/java/com/lucas/redditclone/dto/request/user/SignInRequest.java
+++ src/main/java/com/lucas/redditclone/dto/request/user/SignInRequest.java
@@ -15,8 +15,8 @@ import lombok.NoArgsConstructor;
 public class SignInRequest {
     @NotBlank(message = "the field 'username' is required")
     @Size(min = 6, message = "the field 'username' must be at least {min} characters")
-    @Pattern(regexp = "(?:@)([A-Za-z0-9_](?:(?:[A-Za-z0-9_]|(?:\\.(?!\\.))){0,28}(?:[A-Za-z0-9_]))?)",
-            message = "the field 'username' must be a valid username. Ex: @name.lastname")
+    @Pattern(regexp = "([A-Za-z0-9_](?:(?:[A-Za-z0-9_]|(?:\\.(?!\\.))){0,28}(?:[A-Za-z0-9_]))?)",
+            message = "the field 'username' must be a valid username. Ex: name.lastname")
     private String username;
     @NotBlank(message = "the field 'password' is required")
     @Size(min = 6, max = 48, message = "the field 'password' must be at least {min} characters")
