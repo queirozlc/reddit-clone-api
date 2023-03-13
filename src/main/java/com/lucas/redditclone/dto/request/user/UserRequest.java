16b0c649da2d0c9c8fc6efc2e943194205668605
Lucas :) <91512054+queirozlc@users.noreply.github.com>
2023-03-13 14:58:55 -0300

    refactor: username pattern changes

--- src/main/java/com/lucas/redditclone/dto/request/user/UserRequest.java
+++ src/main/java/com/lucas/redditclone/dto/request/user/UserRequest.java
@@ -16,19 +16,19 @@ import java.time.Instant;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserRequest {
-	@NotBlank(message = "the field 'name' is required")
-	private String name;
-	@NotBlank(message = "the field 'username' is required")
-	@Size(min = 6, message = "the field 'username' must be at least {min} characters")
-	@Pattern(regexp = "(?:@)([A-Za-z0-9_](?:(?:[A-Za-z0-9_]|(?:\\.(?!\\.))){0,28}(?:[A-Za-z0-9_]))?)",
-			message = "the field 'username' must be a valid username. Ex: @name.lastname")
-	private String username;
-	@NotBlank(message = "the field 'email' is required")
-	@Email(message = "the field 'email' must be a valid email")
-	private String email;
-	@NotBlank(message = "the field 'password' is required")
-	@Size(min = 6, max = 48, message = "the field 'password' must be at least {min} characters")
-	private String password;
-	private boolean enabled;
-	private Instant createdAt;
+    @NotBlank(message = "the field 'name' is required")
+    private String name;
+    @NotBlank(message = "the field 'username' is required")
+    @Size(min = 6, message = "the field 'username' must be at least {min} characters")
+    @Pattern(regexp = "([A-Za-z0-9_](?:(?:[A-Za-z0-9_]|(?:\\.(?!\\.))){0,28}(?:[A-Za-z0-9_]))?)",
+            message = "the field 'username' must be a valid username. Ex: name.lastname")
+    private String username;
+    @NotBlank(message = "the field 'email' is required")
+    @Email(message = "the field 'email' must be a valid email")
+    private String email;
+    @NotBlank(message = "the field 'password' is required")
+    @Size(min = 6, max = 48, message = "the field 'password' must be at least {min} characters")
+    private String password;
+    private boolean enabled;
+    private Instant createdAt;
 }
